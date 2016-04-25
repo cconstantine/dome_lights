98e8271700db537ee6a9d4842b4bafa781176824
Chris Constantine <cconstan@gmail.com>
2016-04-24 23:00:47 -0700

    enable glewexperimental

--- main.cpp
+++ main.cpp
@@ -26,13 +26,11 @@ int main( int argc, char** argv )
 		return -1;
 	}
 
-	glfwWindowHint(GLFW_SAMPLES, 1);
-/*
+	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	*/
 
 	// Open a window and create its OpenGL context
 	window = glfwCreateWindow( 800, 800, "Dome Lights", NULL, NULL);
@@ -45,6 +43,8 @@ int main( int argc, char** argv )
 	glfwMakeContextCurrent(window);
 
 	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
